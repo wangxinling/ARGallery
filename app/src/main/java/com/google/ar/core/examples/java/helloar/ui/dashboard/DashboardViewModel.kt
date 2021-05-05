13aa85671230dc4d09eea123db9e57fb2eb9c492
wangxinling <8239838@qq.com>
2021-05-05 10:07:17 +1000

    initial project

---
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.google.ar.core.examples.java.helloar.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
